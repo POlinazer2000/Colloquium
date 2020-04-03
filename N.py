f706d1ff380f8d55d339eac3ae725c591b46383e
Axqa <max-opr@mail.ru>
2020-04-03 14:29:39 +0300

    Немного переделал под условие

--- N.py
+++ N.py
@@ -1,17 +1,17 @@
 class N:
-	def __init__( self, digit ):
-		self.digits = []
-		if isinstance( digit, ( list, tuple ) ):
-			try:
-				self.digits = [ int( i ) for i in digit ]
-			except:
-				raise RuntimeError( "Digit cannot be presented as integer > 0." )
-		elif isinstance( digit, int ):
-			try:
-				self.digits = [ int( i ) for i in str( digit ) ]
-			except:
-				raise RuntimeError( "Digit cannot be presented as integer > 0." )
-		#print( self.digits )
+    def __init__(self, digit):
+        self.digits = []
+        if isinstance(digit, (list, tuple)):
+            try:
+                digit = int("".join( map( str, digit ) )) #костыли, можно и не убирать
+            except:
+                raise RuntimeError("Digit cannot be presented as integer > 0.")
+        if isinstance(digit, int):
+            try:
+                self.digits = [int(i) for i in str(digit)]
+                self.deg = len( str( digit ) )
+            except:
+                raise RuntimeError("Digit cannot be presented as integer > 0.")
 
-	def __str__( self ):
-		return str( ''.join( map( str, self.digits ) ) )
+    def __str__(self):
+        return str(''.join(map(str, self.digits)))
