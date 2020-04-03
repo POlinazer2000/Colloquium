618648227c32cc17a016b5aa9661c1a16585e3ca
Axqa <max-opr@mail.ru>
2020-04-03 14:43:09 +0300

    И Z тогда переделаю. Так будут убираться лишние нули в начале

--- Z.py
+++ Z.py
@@ -1,17 +1,17 @@
 
-class Z( N ):
-    def __init__( self, digit ):
-        digit = str( digit ).replace( '[', '' ).replace( ']', '' ).replace( ' ', '' ).replace( ',', '' )
+class Z(N):
+    def __init__(self, digit):
+        digit = str(int(str(digit).replace('[', '').replace(']', '').replace(' ', '').replace(',', '')))
         try:
             self.sign = False
-            if ( digit[ 0 ] == '-' ):
+            if (digit[0] == '-'):
                 self.sign = True
-                digit = digit[ 1 : ]
-            self.digits = [ int( i ) for i in digit ]
+                digit = digit[1:]
+            self.digits = [int(i) for i in digit]
         except:
-            raise RuntimeError( "Digit cannot be presented as integer." )
+            raise RuntimeError("Digit cannot be presented as integer.")
 
-    def __str__( self ):
-        out = "" if not self.sign else "-" 
-        out += str( ''.join( str( self.digits ) ) )
+    def __str__(self):
+        out = self.sign and '-' or ""
+        out += str(''.join( map( str, self.digits )))
         return out
