99a612447a956eaba60ee328dfe0817f18f8b0c3
MorthimerMcMare <34414934+MorthimerMcMare@users.noreply.github.com>
2020-04-15 15:40:50 +0300

    Добавил GCD и LCM.
    Сделал Владимир. Пока закомментировано.

--- N.py
+++ N.py
@@ -189,3 +189,14 @@ class N:
         else:
             return Z(int(str(''.join(map(str, out)))))'''
 
+    '''def gcd(n1, n2):
+        while N('0') < n1 and N('0') < n2:
+            if(n1 < n2):
+                n2 = n2 % n1
+            else:
+                n1 = n1 % n2
+            return n1 + n2
+    
+    def lcm(n1, n2):
+        return n1 * n2 // gcd(n1, n2)'''
+
