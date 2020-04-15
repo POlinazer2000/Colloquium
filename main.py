d2d2acb19cc9d3d2ad3924cada81b67e59172980
Axqa <max-opr@mail.ru>
2020-04-15 22:41:41 +0300

    тестовая функция

--- main.py
+++ main.py
@@ -3,13 +3,22 @@ from Z import *
 from Q import *
 from poly import *
 
+def tryReverseOp(a, b, op):
+    crutch = {type(N(0))   :1,
+              type(Z(0))   :2,
+              type(Q(0))   :3,
+              type(poly(0)):4
+              }
+
+    if crutch[type(a)] < crutch[type(b)]:
+        eval(type(b)(a) + op + b)
+    else:
+        eval(a + op + type(a)(b))
 
 
 #poly = polynom( [ N([0]), N([3]), N([0]), Z([-1]) ] )
 #print( "f(x) =", poly )
 
-print( N( [ 1, 3 ] ) )
-q1 = Q( Z( [ 2 ] ), N( 1 ) )
+print( tryReverseOp( N(1), N(2), '+' ) )
 
-print("Hello world!")
-q1 = Q( Z( -24 ), N( 142 ) )
+
