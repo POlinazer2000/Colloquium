3d90679a3732b88671bd613698c346b46106f110
MorthimerMcMare <34414934+MorthimerMcMare@users.noreply.github.com>
2020-04-20 12:14:27 +0300

    Перенёс весь одноимённый код в файл poly.py.

--- poly_derivative.py
+++
@@ -1,6 +0,0 @@
-def DER_P_P(poly):
-    poly_derivative=None
-    for i in range (DEG_P_N(poly)):
-        poly_derivative += DEG_P_N(poly) * LED_P_Q(poly)
-        poly.remove(0)
-    return  poly_derivative
