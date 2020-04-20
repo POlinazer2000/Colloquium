b1ee4c2a4c76fc96bb446592b2f2f9ef75af5a3f
MorthimerMcMare <34414934+MorthimerMcMare@users.noreply.github.com>
2020-04-20 19:27:51 +0300

    poly(): дописал адекватный вывод, исправил ошибки.
    Бонусом наш полином умеет производить арифметические операции над мономами с отрицательным показателем степени.

--- poly.py
+++ poly.py
@@ -2,124 +2,198 @@ class poly():
     # Здесь: poly() имеет в себе одну переменную-словарь "coef", в которой хранятся
     #значения по шаблону { <степень>: <Q-объект>, ... }.
     # Пример: poly( {23: Q( 3, 4 ), 7: Q( -4 )} ) будет выглядеть как "3/4 * x^23 - 4 * x^7".
-    def __init__( self, coeflist ):
+    def __init__(self, coeflist):
         self.coef = {}
+
         if isinstance(coeflist, list):
-            for i in range( len( coeflist ) ):
+            for i in range(len(coeflist)):
                 if not isinstance(i, Q):
-                    self.coef.update( { i: Q( coeflist[ i ] ) } ) #append( Q( i ) )
+                    self.coef.update({i: Q(coeflist[i])})
                 else:
-                    self.coef.update( { i: coeflist[ i ] } )
+                    self.coef.update({i: coeflist[i]})
         elif isinstance(coeflist, dict):
-            self.coef = coeflist
+            self.coef = coeflist.copy()
+        elif isinstance(coeflist,(Z,N)):
+            self.coef = {0: coeflist.toQ()}
+        elif isinstance(coeflist, Q):
+            self.coef = {0: coeflist}
+        elif isinstance(coeflist, str):
+            lst = coeflist.split(' ')
+            for i in range(len(lst)):
+                if not isinstance(i, Q):
+                    self.coef.update({i: Q(lst[i])})  # append( Q( i ) )
+                else:
+                    self.coef.update({i: lst[i]})
+        coeflist = self.coef.copy()
+        for i in self.coef.keys():
+            if str( self.coef[i] ) == "0": # A-a-argh, beautiful crutch!..
+                coeflist.pop(i)
+        self.coef = coeflist.copy()
+
+        if len(self.coef) == 0:
+            self.coef.update( {0: Q(0)} )
 
     def __str__(self):
         coef, out = self.coef, ""
-        if len( self.coef ) == 0:
+        if len( self ) == 0: # Not "self.deg()" (see "poly({ 0: Q( 1 ) })").
             out = "0"
         else:
-            out = str( self.coef.items() )
-            #for i in range( len( self.coef ) ):
-                #out += str( self.coef[ i ] ) + ", "
-            #out = str( self.coef )
-            '''for i in range( len( self.coef ) ):
-                fl = poz_z_d( coef[i].num )
-                if fl == 0:
-                    pass
+            i = 0
+            powers = list( self.coef.keys() )
+            coefs = list( self.coef.values() )
+            while ( len( powers ) != 0 ):
+                maxpowerindex = powers.index( max( powers ) )
+                maxpower = powers.pop( maxpowerindex )
+                outcoef = maxpwrcoef = coefs.pop( maxpowerindex )
+                if ( str( maxpwrcoef ) == "1" ):
+                    outcoef = ""
+                elif ( str( maxpwrcoef ) == "-1" ):
+                    outcoef = "-"
+                #print( "Power", maxpower, "at", maxpowerindex, "with coefficient", maxpwrcoef )
+                if not maxpwrcoef.num.sign and i != 0:
+                    out += '+'
+                if i == self.deg() - 1:
+                    out += str( outcoef ) + 'x'
+                elif i == self.deg():
+                    out += str( maxpwrcoef ) if isinstance( outcoef, str ) else str( outcoef )
                 else:
-                    if i != 0 and not coef[i].num.sign:
-                        out += '+'
-                    if i == self.deg-2:
-                        out += str( coef[i] ) + 'x'
-                    elif i == self.deg - 1:
-                        out += str( coef[i] )
-                    else:
-                        out += str( coef[i] ) +'x^'+ str( self.deg - i - 1)'''
-        
+                    out += str( outcoef ) +'x^'+ str( maxpower )
+                i += 1
         return out
 
-    def __add__(self, other):
-        if len(self.coef)<len(other.coef):
-            self, other = other, self
-        out=poly(str(self))
-        do=len(other.coef)
-        ds=len(self.coef)
-        for i in range(do):
-            out.coef[ds-do+i] += other.coef[i]
-        while(out.coef[0]==0):
-            out.coef.pop(0)
-        return poly(str(out))
+    # Степень и длина списка коэффициентов многочлена.
+    def __len__( self ):
+        return len( list( self.coef.keys() ) )
+    def deg( self ):
+        return max( list( self.coef.keys() ) )
 
+    # Ведущий (самый старший) аргумент
+    def lead(self):
+        return self.coef[self.deg()]
+    
+    
+    # Перегрузка операторов.
+    
+    def __add__(self, other):
+        if type(self) != type(other):
+            return tryReverseOp(self, other, '+')
+        tmp = self.coef
+        for i in other.coef:
+            if i in tmp:
+                tmp[i] = tmp[i]+ other.coef[i]
+            else:
+                tmp.update({i:other.coef[i]})
+        return poly(tmp)
 
     def __sub__(self, other):
-        if len(self.coef) < len(other.coef):
-            self, other = other, self
-        out = poly(str(self))
-        do = len(other.coef)
-        ds = len(self.coef)
-        for i in range(do):
-            out.coef[ds - do + i] -= other.coef[i]
-        while (out.coef[0] == 0):
-            out.coef.pop(0)
-        return poly(str(out))
+        if type(self) != type(other):
+            return tryReverseOp(self, other, '-')
+        tmp = self.coef
+        for i in other.coef:
+            if i in tmp:
+                tmp[i] = tmp[i] - other.coef[i]
+            else:
+                tmp.update({i: Z(-1)*other.coef[i]})
+        return poly(tmp)
 
+    def mulq(self,q):
+        out = poly(self.coef)
+        for i in out.coef:
+            out[i] = out[i]*q
+        return out
+
+    def mulqx(self,q,k):
+        key = self.coef.keys()
+        out = {}
+        for i in key:
+            out[i+k] = self.coef[i]*q
+        return poly(out)
 
     def __mul__(self, other):
-        if len(self.coef) < len(other.coef):
+        if type(self) != type(other):
+            return tryReverseOp(self, other, '*')
+        if deg(self) < deg(other):
             self, other = other, self
-        out=poly(str([0]*(len(self.coef)+len(other.coef)-2)))
-        for i in range(len(self.coef)):
-            for j in range(len(other.coef)):
-                out.coef[i+j]+= self.coef[i] * other.coef[j]
-        return poly(str(out))
-
-
-    def __floordiv__(self, other):
-        outq=poly(str([0]*(len(self.coef)-len(other.coef))))
-        outr=poly(str(self.coef))
-        i=0
-        while outr >= other:
-            outq.coef[i]=outr.coef[0]
-            outr-=other*outq
-            i+=1
-        return poly(str(outq))
-
-
-    def __mod__(self, other):
-        outq=poly(str([0]*(len(self.coef)-len(other.coef))))
-        outr=poly(str(self.coef))
-        i=0
-        while outr >= other:
-            outq.coef[i]=outr.coef[0]
-            outr-=other*outq
-            i+=1
-        return poly(str(outr))
+        out = poly(0)
+        for i in other.coef.keys():
+            out = out + self.mulqx(other.coef[i],i)
+        return out
+    '''
+        out = {}
+        for i in self.coef:
+            for j in other.coef:
+                if i+j in out:
+                    out[i+j] = out[i+j] + self.coef[i] * other.coef[j]
+                else:
+                    out.update({i+j:self.coef[i] * other.coef[j]})
+        return poly(out)'''
 
     def factor_P(self):
-        a=poly(str(self))
+        a=poly(self.coef)
         g=abs(a.coef[0].num).GCF(abs(a.coef[0].num))
         l=a.coef[0].denum.LCM(a.coef[0].denum)
         for i in range(1, len(a.coef)):
             g=gcd.GCF(abs(a.coef[0].num))
             l=lcm.LCM(a.coef[i].denum)
-        return Q(str(g), str(l))
+        return Q(g, l)
 
-    # НОД многочлена.
-    def GCF_PP_P(poly1,poly2):
-        c=poly1%poly2
-        while NZER_N_B(DEG_P_N(c)):
-            poly1,poly2=poly2,c
-            c=poly1%poly2
+    #вернет целую часть
+    def __truediv__(self, other):
+        if type(self) != type(other):
+            return tryReverseOp(self, other, '/')
+        p1 = poly(self.coef)
+        p2 = poly(other.coef)
+        res = poly(0)
+        while (p1.deg() > p2.deg ):
+            dif = p1.deg()-p2.deg()
+            q = p1.coef[p1.deg()]/p2.coef[p2.deg()]
+            res = res + poly({dif:q})
+            p1 = p1-p2.mulqx(q, dif )
+        r = p1
+        return res
+
+    def __mod__(self, other):
+        if type(self) != type(other):
+            return tryReverseOp(self, other, '%')
+        p1 = poly(self.coef)
+        p2 = poly(other.coef)
+        res = poly(0)
+        while (p1.deg() > p2.deg() ):
+            dif = p1.deg()-p2.deg()
+            q = p1.coef[p1.deg()]/p2.coef[p2.deg()]
+            res = res + poly({dif:q})
+            p1 = p1-p2.mulqx(q, dif )
+        return p1
+
+    def __floordiv__(self, other):
+        if type(self) != type(other):
+            return tryReverseOp(self, other, '//')
+        return self / other
+
+    def gcd(self,other):
+        c = self % other
+        poly1 = poly(self.coef)
+        poly2 = poly(other.coef)
+        while c.deg()>0:
+            poly1, poly2=poly2, c
+            c = poly1 % poly2
         return poly2
 
-    # Производная. Жаль, нет такого оператора в Python, перегрузить не получится...
-    def DER_P_P(poly):
-        poly_derivative=None
-        for i in range (DEG_P_N(poly)):
-            poly_derivative += DEG_P_N(poly) * LED_P_Q(poly)
-            poly.remove(0)
-        return  poly_derivative
+    # Производная многочлена.
+    def der(self):
+        poly_derivative = poly(0)
+        pol = poly(self.coef)
+        for i in pol.coef:
+            if i:
+                poly_derivative = poly_derivative + poly({i-1: Q(i)*pol.coef[i]})
+        return poly_derivative
 
-    
-#p = poly( [ Q( 2 ), Q(3), Q(4) ] )
-#print( p )
+    #кратные корни в простые
+    def nmr(self):
+        der = self.der()
+        gcd = self.gcd(der)
+        res = self / gcd
+        return res
+
+
+#print( poly({  -4: Q( 5, 4 )  }) + poly({  -4: Q( 7, 17 ), 17: Q( -3 )  }) )
